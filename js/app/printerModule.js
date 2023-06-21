739a9f9b2691f4c33dd3f5b9cc56940a2ea0b5a8
Zeev Rotshtein <zeev.rotshtein@perception-point.io>
2023-06-22 00:54:29 +0300

    Added mangled print results

--- js/app/printerModule.js
+++ js/app/printerModule.js
@@ -1,15 +1,15 @@
-define(['jquery'], function($) {
-	printed_content = $('#printed-content');
+define([], function() {
+	printed_content = document.getElementById('printed-content');
 
 	var printerModuleInitialize = function() {
 	};
 
 	var printerModulePrint = function() {
-		printed_content.addClass('printed');
+		printed_content.classList.add('printed');
 	};
 
 	var printerModuleReset = function() {
-		printed_content.removeClass('printed');
+		printed_content.classList.remove('printed');
 	};
 
 	return {
