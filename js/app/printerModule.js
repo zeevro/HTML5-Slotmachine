2fbbdcc2ee12421dc374119980d0370afa99be0b
Zeev Rotshtein <zeev.rotshtein@perception-point.io>
2023-06-22 01:59:02 +0300

    Randomize mangled prints

--- js/app/printerModule.js
+++ js/app/printerModule.js
@@ -1,10 +1,14 @@
 define([], function() {
 	printed_content = document.getElementById('printed-content');
+	filter_saturation = document.getElementById('printerFilterSaturation');
+	filter_turbulence = document.getElementById('printerFilterTurbulence');
 
 	var printerModuleInitialize = function() {
 	};
 
 	var printerModulePrint = function() {
+		filter_saturation.setAttribute('values', Math.random() * 0.6)
+		filter_turbulence.setAttribute('seed', Math.random() * 100000)
 		printed_content.classList.add('printed');
 	};
 
