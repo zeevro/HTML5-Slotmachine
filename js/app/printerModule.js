64212caebbe5a1b38f36799abcd5b05d17ef2e0c
Zeev Rotshtein <zeevro@gmail.com>
2023-06-23 18:10:12 +0300

    Replace printed image with random cat picture

--- js/app/printerModule.js
+++ js/app/printerModule.js
@@ -1,5 +1,6 @@
 define([], function() {
 	printed_content = document.getElementById('printed-content');
+	printer_image = document.getElementById('printerImage');
 	filter_saturation = document.getElementById('printerFilterSaturation');
 	filter_turbulence = document.getElementById('printerFilterTurbulence');
 
@@ -9,6 +10,7 @@ define([], function() {
 	var printerModulePrint = function() {
 		filter_saturation.setAttribute('values', Math.random() * 0.6)
 		filter_turbulence.setAttribute('seed', Math.random() * 100000)
+		printer_image.setAttribute('href', 'https://cataas.com/cat?width=286&height=180');
 		printed_content.classList.add('printed');
 	};
 
